b491da0253799fd06c57de7601a03aeb7239d9d4
Kevin Rengifo <krengifo@unimayor.edu.co>
2026-09-15 23:30:06 -0500

    Mi primer commit

---
+++ backend/server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const cors = require("cors");
+
+const conectarDB = require("./config/database");
+const usuarioRoutes = require("./routes/usuarioRoutes");
+const favoritoRoutes = require("./routes/favoritoRoutes")
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// Conectar a MongoDB
+conectarDB();
+
+// Rutas
+app.use("/api/usuarios", usuarioRoutes);
+app.use("/api/favoritos", favoritoRoutes);
+
+// Ruta de prueba
+app.get("/", (req, res) => {
+    res.json({
+        mensaje: "Servidor funcionando correctamente"
+    });
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log('Servidor ejecutándose en http://localhost:${PORT}');
+});
